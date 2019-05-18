55c568746f9a9fa0172fc1c86c821fb7d8ac3724
jesus9233 <jimyy9211@gmail.com>
2019-05-18 22:11:15 +0530

    main push

--- ShoeStore/settings.py
+++ ShoeStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-fncwk1nuwlk%y5^=z_g1vb$qo@v&t@q#e$flp8%ttw1d=9f!b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['my-shoestore.herokuapp.com', 'localhost']
 
