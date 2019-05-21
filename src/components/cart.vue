966feddc33e0396743941a3da571786d4f45a839
jesus9233 <jimyy9211@gmail.com>
2019-05-21 14:03:50 +0530

    fixed loading issue

--- src/components/cart.vue
+++ src/components/cart.vue
@@ -1,125 +1,126 @@
 <template>
-	<div>
-		<app-header>my cart</app-header>
-		<div class="container-fluid pt-5 cart">
-			<div class="container pt-5" >
-				<div class="row">
-					<div class="col-lg-7 cart-items align-self-top mb-5">
-						<div class="border p-3">
-							<h4 class="p-1" v-if="products">My Cart [ {{products.length}} ]</h4>
-							<hr>
-							<div v-if="products.length < 1">
-								Buy Something <router-link :to="{name:'Shop'}" class="text-primary">Here</router-link>
-							</div>
-							<div class="row my-3 border-bottom p-1" v-for="product in products" v-else>
-								<div class="col-sm-5">
-									<router-link :to="'/product/'+product.slug">
-										<img :src="product.thumb" alt="product thumb">
-									</router-link>
-								</div>
-								<div class="col-sm-7">
-									<h5>
-										<router-link :to="'/product/'+product.slug">
-											{{product.name}}
-										</router-link>
-									</h5>
-									<span>Rs. {{product.price}}</span>
-									<p>Size {{product.size}}</p>
-									<div class="text-right">
-										<a class="btn btn-danger btn-sm text-white" @click.prevent="cartToggle(product.id)">
-											<font-awesome-icon icon="trash-alt" class="mr-1" />Remove
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-5 total-cart" v-if="products.length > 0">
-						<div class="border p-3" v-if="cart">
-							<h4 class="p-1">Cart Totals</h4>
-							<hr>
-							<p class="d-flex p-2">
-								<span>Items Count</span>
-								<span>{{cart.products.length}}</span>
-							</p>
-							<p class="d-flex p-2">
-								<span>Subtotal</span>
-								<span>Rs {{cart.subtotal}}</span>
-							</p>
-							<p class="d-flex p-2 total border-top">
-								<span>Total</span>
-								<span>Rs {{cart.total}}</span>
-							</p>
-						</div>
-						<div class="text-right">
-							<router-link class="btn btn-success m-2" tag="button" :to="{name:'Checkout'}" exact>
-								Checkout
-							</router-link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+
+  <div>
+    <app-header>my cart</app-header>
+    <div class="container-fluid pt-5 cart">
+      <div class="container pt-5">
+        <div class="row" v-if="products">
+          <div class="col-lg-7 cart-items align-self-top mb-5">
+            <div class="border p-3">
+              <h4 class="p-1">My Cart [ {{products.length}} ]</h4>
+              <hr>
+              <div v-if="products.length < 1">
+                Buy Something
+                <router-link :to="{name:'Shop'}" class="text-primary">Here</router-link>
+              </div>
+              <div class="row my-3 border-bottom p-1" v-for="product in products" v-else>
+                <div class="col-sm-5">
+                  <router-link :to="'/product/'+product.slug">
+                    <img :src="product.thumb" alt="product thumb">
+                  </router-link>
+                </div>
+                <div class="col-sm-7">
+                  <h5>
+                    <router-link :to="'/product/'+product.slug">
+                      {{product.name}}
+                    </router-link>
+                  </h5>
+                  <span>Rs. {{product.price}}</span>
+                  <p>Size {{product.size}}</p>
+                  <div class="text-right">
+                    <a class="btn btn-danger btn-sm text-white" @click.prevent="cartToggle(product.id)">
+                      <font-awesome-icon icon="trash-alt" class="mr-1" />Remove
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-5 total-cart" v-if="products.length > 0">
+            <div class="border p-3" v-if="cart">
+              <h4 class="p-1">Cart Totals</h4>
+              <hr>
+              <p class="d-flex p-2">
+                <span>Items Count</span>
+                <span>{{cart.products.length}}</span>
+              </p>
+              <p class="d-flex p-2">
+                <span>Subtotal</span>
+                <span>Rs {{cart.subtotal}}</span>
+              </p>
+              <p class="d-flex p-2 total border-top">
+                <span>Total</span>
+                <span>Rs {{cart.total}}</span>
+              </p>
+            </div>
+            <div class="text-right">
+              <router-link class="btn btn-success m-2" tag="button" :to="{name:'Checkout'}" exact>
+                Checkout
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 <script>
-	import Header from './header/header.vue';
 
-	export default{
-		data(){
-			return{
-			}
-		},
-		components:{
-			appHeader:Header
-		},
-		computed:{
-			products(){
-				return this.$store.state.cartItems.variants;
-			},
-			cart(){
-				return this.$store.state.cartItems.cart;
-			}
-		},
-		methods:{
-			cartToggle(productId){
-				this.$store.dispatch('cartToggle', productId);
-			},
-		}
-	}
+  import Header from './header/header.vue'
+
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      appHeader: Header
+    },
+    computed: {
+      products() {
+        return this.$store.state.cartItems.variants
+      },
+      cart() {
+        return this.$store.state.cartItems.cart
+      }
+    },
+    methods: {
+      cartToggle(productId) {
+        this.$store.dispatch('cartToggle', productId)
+      }
+    }
+  }
+
 </script>
 <style scoped>
 
-a{
-	color:#333;
-}
+  a {
+    color: #333;
+  }
 
-	.head{
-		font-size: 1.2rem;
-		padding: 1rem;
-	}
-	.cart img{
-		max-width:100%;
-	}
-	.cart input.form-control{
-		max-width: 100px;
-		min-width: 70px;
-	}
-	/* .cart div{
-		align-items: center;
-	} */
+  .head {
+    font-size: 1.2rem;
+    padding: 1rem;
+  }
+  .cart img {
+    max-width: 100%;
+  }
+  .cart input.form-control {
+    max-width: 100px;
+    min-width: 70px;
+  }
+  .total-cart span {
+    display: block;
+    width: 50%;
+  }
+  .total {
+    font-size: 1.2rem;
+  }
+  .cart-items {
+    overflow-x: auto;
+  }
+  .border-bottom:last-of-type {
+    border-bottom: none !important;
+  }
 
-	.total-cart span{
-		display: block;
-		width: 50%;
-	}
-	.total{
-		font-size: 1.2rem;
-	}
-	.cart-items{
-		overflow-x: auto;
-	}
-	.border-bottom:last-of-type{
-		border-bottom:none !important;
-	}
 </style>
--- src/components/single.vue
+++ src/components/single.vue
@@ -1,187 +1,194 @@
 <template>
+
   <div>
     <app-header>Single</app-header>
     <div class="pt-5">
       <div class="container">
-        <div class="row pb-5 text-capitalize">
-          <div class="col-lg-4">
-            <div class="image">
-              <img v-if="product" :src="image ? image :  product.product.thumb" alt="product image">
+        <template v-if="product">
+          <div class="row pb-5 text-capitalize">
+            <div class="col-lg-4">
+              <div class="image">
+                <img :src="image ? image :  product.product.thumb" alt="product image">
+              </div>
             </div>
-          </div>
-          <div class="col-lg-8">
-            <h3 class="poppins" v-if="product">{{product.product.title}}</h3>
-            <span v-if="product">{{fetchMain}}</span><div>
-              <p class="font-weight-bold d-inline-block" v-if="product">{{price ? price : product.product.price}}</p>
-              <span class="text-monospace"></span>
-            </div>
-            <div class="my-3">
-                <label>Color</label>
-                <div v-if="product">
-                  <div v-for="child in product.children"
-                   class="select-color" :class="{'selectedColor':child.is_main}">
-                     <img :src="child.image" @click="childChanger(child.color)"
-                      alt="color"
-                      />
+            <div class="col-lg-8">
+              <h3 class="poppins">{{product.product.title}}</h3>
+              <span>{{fetchMain}}</span><div>
+                <p class="font-weight-bold d-inline-block">{{price ? price : product.product.price}}</p>
+                <span class="text-monospace"></span>
+              </div>
+              <div class="my-3">
+                  <label>Color</label>
+                  <div>
+                    <div v-for="child in product.children"
+                     class="select-color" :class="{'selectedColor':child.is_main}">
+                       <img :src="child.image" @click="childChanger(child.color)"
+                        alt="color"
+                        />
+                    </div>
                   </div>
-                </div>
-            </div>
-            <div class="form-group">
-              <label for="id_size">Size</label>
-              <select class="custom-select" id="id_size" ref="selectedSize" @input="getQty">
-                <option value="undefined" selected>Select Size</option>9
-                <option v-for="variant in variants" :value="variant.size">{{variant.size}}</option>
-              </select>
-            </div>
-            <h5 :class="qtyClass" v-if="qty !== '' ">{{qtyText}}</h5>
-            <p v-if="product">Department : {{product.product.gender}}</p>
-            <p v-if="product">Category : {{product.product.category}}</p>
-            <p v-if="product">Brand : {{product.product.brand}}</p>
-            <p v-if="product">Material : {{product.product.material_type}}</p>
-            <div v-if="product && $store.state.cartItems">
-              <button :class="inCart(variantId) ? 'btn-danger' :'btn-success'"
-              @click="cartToggle(variantId)"  type="button" class="btn"
-              :disabled="(variantId === '' || qty === 0) && !inCart(variantId) && cart">
-                <span v-if="product && inCart(variantId)">Remove from Cart</span>
-                <span v-else>Add to Cart</span>
-              </button>
+              </div>
+              <div class="form-group">
+                <label for="id_size">Size</label>
+                <select class="custom-select" id="id_size" ref="selectedSize" @input="getQty">
+                  <option value="undefined" selected>Select Size</option>9
+                  <option v-for="variant in variants" :value="variant.size">{{variant.size}}</option>
+                </select>
+              </div>
+              <h5 :class="qtyClass" v-if="qty !== '' ">{{qtyText}}</h5>
+              <p>Department : {{product.product.gender}}</p>
+              <p>Category : {{product.product.category}}</p>
+              <p>Brand : {{product.product.brand}}</p>
+              <p>Material : {{product.product.material_type}}</p>
+              <div v-if="$store.state.cartItems">
+                <button :class="inCart(variantId) ? 'btn-danger' :'btn-success'"
+                @click="cartToggle(variantId)"  type="button" class="btn"
+                :disabled="(variantId === '' || qty === 0) && !inCart(variantId) && cart">
+                  <span v-if="product && inCart(variantId)">Remove from Cart</span>
+                  <span v-else>Add to Cart</span>
+                </button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div>
-          <b-tabs content-class="mt-3">
-            <b-tab title="Description" active v-if="product"><p>{{product.product.description}}</p></b-tab>
-            <b-tab title="Reviews"><p>This is Review</p></b-tab>
-          </b-tabs>
-        </div>
+          <div>
+            <b-tabs content-class="mt-3">
+              <b-tab title="Description" active><p>{{product.product.description}}</p></b-tab>
+              <b-tab title="Reviews"><p>This is Review</p></b-tab>
+            </b-tabs>
+          </div>
+        </template>
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
-import Header from './header/header.vue'
-import axios from 'axios';
 
-export default{
-  data() {
-    return {
-      image:'',
-      variants:'',
-      price:'',
-      qty:'',
-      qtyText:'',
-      qtyClass:'',
-      variantId:'',
-      cart:''
-    }
-  },
-  components:{
-    appHeader:Header
-  },
-  computed:{
-    product(){
-      var product = this.$store.state.productDetail;
-      if(product){
-        return product;
+  import Header from './header/header.vue'
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        image: '',
+        variants: '',
+        price: '',
+        qty: '',
+        qtyText: '',
+        qtyClass: '',
+        variantId: '',
+        cart: ''
       }
-      return null;
     },
-    fetchMain(){
-      var obj = this.$store.state.productDetail.children;
-      for (var i = 0; i < obj.length; i++) {
-        if(obj[i].is_main == true){
-          this.fetchVariant(obj[i].id, obj[i].color);
+    components: {
+      appHeader: Header
+    },
+    computed: {
+      product() {
+        var product = this.$store.state.productDetail
+        if (product) {
+          return product
         }
-      }
-    }
-  },
-  methods:{
-    fetchProduct(){
-      var slug = this.$route.params.slug;
-      this.$store.commit('getProductDetail',slug);
-    },
-    fetchVariant(id,color){
-      var color = color.replace('#','');
-      this.$store.state.loading = true;
-      axios.get(process.env.API_URL+'/api/variants/'+id+'/'+color)
-      .then((response) => {
-        this.variants = response.data;
-        this.$store.state.loading = false;
-        this.$refs.selectedSize.value=undefined;
-        this.qty=''
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    },
-    cartToggle(productId){
-        this.cart = true;
-        this.$store.dispatch('cartToggle', productId).then(this.cart = false)
-    },
-    inCart(productId){
-      return this.$store.state.cartItems.cart.products.includes(productId);
-    },
-    childChanger(color){
-      let el = event.srcElement.parentNode;
-      for (var i = 0; i < el.parentNode.childNodes.length; i++) {
-        el.parentNode.childNodes[i].classList.remove('selectedColor')
-      }
-      el.classList.add('selectedColor');
-      var product = this.$store.state.productDetail.children;
-      for (var i = 0; i < product.length; i++) {
-        if(product[i].color == color){
-              this.image = product[i].image;
-              this.price = product[i].price;
-              var id = product[i].id;
-              this.fetchVariant(id, color);
-              this.variantId='';
-          }
-      }
-    },
-    getQty(){
-      var size = this.$refs.selectedSize.value;
-      for (var i = 0; i < this.variants.length; i++) {
-        if(this.variants[i].size == size){
-          this.qty = this.variants[i].quantity;
-          this.variantId = this.variants[i].id;
-          if(this.qty > 10){
-            this.qtyClass = 'text-success';
-            this.qtyText = 'In stock';
-          }
-          else if(this.qty <= 10 && this.qty > 0){
-            this.qtyClass = 'text-warning';
-            this.qtyText = 'Only '+this.qty+' left';
-          }
-          else{
-            this.qtyClass = 'text-danger';
-            this.qtyText = 'Out of stock';
+        return null
+      },
+      fetchMain() {
+        var obj = this.$store.state.productDetail.children
+        for (var i = 0; i < obj.length; i++) {
+          if (obj[i].is_main == true) {
+            this.fetchVariant(obj[i].id, obj[i].color)
           }
         }
       }
     },
-  },
-  mounted(){
-    this.fetchProduct();
+    methods: {
+      fetchProduct() {
+        var slug = this.$route.params.slug
+        this.$store.commit('getProductDetail', slug)
+      },
+      fetchVariant(id, color) {
+        var color = color.replace('#', '')
+        this.$store.state.loading = true
+        axios
+          .get(process.env.API_URL + '/api/variants/' + id + '/' + color)
+          .then(response => {
+            this.variants = response.data
+            this.$store.state.loading = false
+            this.$refs.selectedSize.value = undefined
+            this.qty = ''
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      },
+      cartToggle(productId) {
+        this.cart = true
+        this.$store.dispatch('cartToggle', productId).then((this.cart = false))
+      },
+      inCart(productId) {
+        return this.$store.state.cartItems.cart.products.includes(productId)
+      },
+      childChanger(color) {
+        let el = event.srcElement.parentNode
+        for (var i = 0; i < el.parentNode.childNodes.length; i++) {
+          el.parentNode.childNodes[i].classList.remove('selectedColor')
+        }
+        el.classList.add('selectedColor')
+        var product = this.$store.state.productDetail.children
+        for (var i = 0; i < product.length; i++) {
+          if (product[i].color == color) {
+            this.image = product[i].image
+            this.price = product[i].price
+            var id = product[i].id
+            this.fetchVariant(id, color)
+            this.variantId = ''
+          }
+        }
+      },
+      getQty() {
+        var size = this.$refs.selectedSize.value
+        for (var i = 0; i < this.variants.length; i++) {
+          if (this.variants[i].size == size) {
+            this.qty = this.variants[i].quantity
+            this.variantId = this.variants[i].id
+            if (this.qty > 10) {
+              this.qtyClass = 'text-success'
+              this.qtyText = 'In stock'
+            } else if (this.qty <= 10 && this.qty > 0) {
+              this.qtyClass = 'text-warning'
+              this.qtyText = 'Only ' + this.qty + ' left'
+            } else {
+              this.qtyClass = 'text-danger'
+              this.qtyText = 'Out of stock'
+            }
+          }
+        }
+      }
+    },
+    mounted() {
+      this.fetchProduct()
+    }
   }
-}
 
 </script>
 <style scoped>
-  .select-color{
-    height:5rem;
-    width:5rem;
-    margin:0rem 0.5rem;
-    cursor:pointer;
-    border:1px solid;
-    display:inline-block;
+
+  .select-color {
+    height: 5rem;
+    width: 5rem;
+    margin: 0rem 0.5rem;
+    cursor: pointer;
+    border: 1px solid;
+    display: inline-block;
   }
 
-  .image img, .select-color img{
-    max-width:100%;
+  .image img,
+  .select-color img {
+    max-width: 100%;
   }
-  .selectedColor{
-    outline:3px solid #00FF7F;
+  .selectedColor {
+    outline: 3px solid #00ff7f;
   }
+
 </style>
